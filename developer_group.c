be494faa10dcfd4c97b172eff19248a0b7ab2e68
lucadiener <122467797+lucadiener@users.noreply.github.com>
2023-03-13 20:59:33 +0000

    first structure of the developer_group program
    inits have to be finished and pointers to the developer function are still missing

---
+++ developer_group.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <developer.h>
+
+
+#define LOGO_LENGTH 100
+
+typedef struct 
+{
+    developer dev[2];
+    char group_logo[LOGO_LENGTH];
+}developer_group;
+
+
+void developer_group_init(developer *const d, char group_logo[])
+{
+group_logo_init()
+{
+
+strncpy(d->group_logo, logo, logo[]+1)
+
+printf(Das Gruppenlogo ist %c, )
+};
+
+
+
+}
+
+void developer_group_print()
+{
+    print_group();
+    print_logo();
+
+}
