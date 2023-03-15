b13b95e952304cc2e3bdd81ec53b628e57b70907
DennisDick <122461822+DennisDick@users.noreply.github.com>
2023-03-15 10:12:55 +0000

    finished header file for developer
    the header file developer.h is finished and has include guards

--- developer.h
+++ developer.h
@@ -1,3 +1,7 @@
+#ifndef DEVELOPER_H_hfw308iohea
+#define DEVELOPER_H_hfw308iohea
+
+
 #define NAME_LENGTH 20
 
 typedef struct
@@ -6,4 +10,8 @@ typedef struct
     char alias[NAME_LENGTH];
 } developer;
 
-void developer_init( developer *const d, const char name[], const char alias[]);
+void developer_init(developer *const dev, const char name[], const char alias[]);
+
+void developer_print(const developer *const dev);
+
+#endif
