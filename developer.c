c0c4ac300a7613a85b784ff3e8c5aa1f3d8235f8
DennisDick <122461822+DennisDick@users.noreply.github.com>
2023-03-15 10:16:25 +0000

    finished developer.c file
    finished developer.c file which may need some rework later but overall the file is ready to work with

--- developer.c
+++ developer.c
@@ -2,28 +2,34 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define NAME_LENGTH 20
+#define NUMBER_DEVELOPER 2
 
-void developer_init( developer *const d, const char name[], const char alias[])         //initialization of developers 
+void developer_init( developer *const dev, const char name[], const char alias[])         //initialization of developers 
 {
-    strncpy(d->name, name, strlen(name) + 1);
-    strncpy(d->alias, alias, strlen(alias) + 1);
+    strncpy(dev->name, name, strlen(name) + 1);
+    strncpy(dev->alias, alias, strlen(alias) + 1);
 }
 
-void developer_print( developer const *const d)         //function for printing developers
+void developer_print(const developer *const dev)         //function for printing developers with pointer which points on array of developer in structure developer_group
 {
-    printf("*************************\n");         
-    printf("========================\n");         
-    printf("Developer: \n");
-    printf("%c\t", d->name);            //developer array of Luca has to be inserted in that line
-    printf("%c\t", d->alias);           //developer array of Luca has to be inserted in that line
-    printf("========================\n\n");
-    printf("========================\n");         
-    printf("Developer: \n");
-    printf("%c\t", d->name);            //developer array of Luca has to be inserted in that line
-    printf("%c\t", d->alias);           //developer array of Luca has to be inserted in that line
-    printf("========================\n\n");
-    printf("*************************\n");         
+    if (dev == NULL)
+    {
 
+    }
+    else
+    {
+        printf("*************************\n"); 
+        for(int i = 0; i < NUMBER_DEVELOPER; i++)           //loop to print all developers 
+        {
+            printf("========================\n");         
+            printf("Developer: \n");
+            printf("%s\t", dev[i].name);            //printing developer name and alias 
+            printf("%s\t", dev[i].alias);           
+            printf("========================\n\n");
+        }        
+        printf("*************************\n");
+    }       
 }
